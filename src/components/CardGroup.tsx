6ccfbe56004dccb0af52be409ce5a819081662c9
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-14 10:48:53 +0630

    card component

--- src/components/CardGroup.tsx
+++ src/components/CardGroup.tsx
@@ -10,15 +10,37 @@ function CardGroup() {
 
   return (
     <Fragment>
-      <div className="example-card">
-        <h2>Card Heading 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          autem vel odit asperiores quae eveniet reprehenderit obcaecati, ipsum
-          consectetur sunt temporibus nam, ab possimus aspernatur repudiandae
-          cumque amet quia numquam!
-        </p>
-        <button className="primary">ClickMe</button>
+      <div className="row">
+        <div className="example-card col-4" style={style}>
+          <h2>Card Heading 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            autem vel odit asperiores quae eveniet reprehenderit obcaecati,
+            ipsum consectetur sunt temporibus nam, ab possimus aspernatur
+            repudiandae cumque amet quia numquam!
+          </p>
+          <button className="btn btn-primary">ClickMe</button>
+        </div>
+        <div className="example-card col-4" style={style}>
+          <h2>Card Heading 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            autem vel odit asperiores quae eveniet reprehenderit obcaecati,
+            ipsum consectetur sunt temporibus nam, ab possimus aspernatur
+            repudiandae cumque amet quia numquam!
+          </p>
+          <button className="btn btn-primary">ClickMe</button>
+        </div>
+        <div className="example-card col-3" style={style}>
+          <h2>Card Heading 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            autem vel odit asperiores quae eveniet reprehenderit obcaecati,
+            ipsum consectetur sunt temporibus nam, ab possimus aspernatur
+            repudiandae cumque amet quia numquam!
+          </p>
+          <button className="btn btn-primary">ClickMe</button>
+        </div>
       </div>
     </Fragment>
   );
