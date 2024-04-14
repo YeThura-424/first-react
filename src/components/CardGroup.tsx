faa70c4ae5c297b1d35f853e24a8e095208f7dbb
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-14 14:47:57 +0630

    card

--- src/components/CardGroup.tsx
+++ src/components/CardGroup.tsx
@@ -5,43 +5,38 @@ function CardGroup() {
     margin: "16px",
     padding: "16px",
     borderRadius: "5px",
-    boxShadow: "2px 0px 3px #eed44a",
+    boxShadow: "0px 0px 9px 4px #ccc4f0",
   };
+  const cardArr = [
+    {
+      title: "Block Title 1",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritati autem vel odit asperiores quae eveniet reprehenderit obcaecati",
+    },
+    {
+      title: "Block Title 2",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritati autem vel odit asperiores quae eveniet reprehenderit obcaecati",
+    },
+    {
+      title: "Block Title 3",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritati autem vel odit asperiores quae eveniet reprehenderit obcaecati",
+    },
+  ];
 
+  const blockCards = cardArr.map((blog, index) => {
+    return (
+      <div className="example-card col-3" style={style} key={index}>
+        <h2>{blog.title}</h2>
+        <p>{blog.description}</p>
+        <button className="btn btn-primary">ClickMe</button>
+      </div>
+    );
+  });
   return (
     <Fragment>
-      <div className="row">
-        <div className="example-card col-4" style={style}>
-          <h2>Card Heading 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            autem vel odit asperiores quae eveniet reprehenderit obcaecati,
-            ipsum consectetur sunt temporibus nam, ab possimus aspernatur
-            repudiandae cumque amet quia numquam!
-          </p>
-          <button className="btn btn-primary">ClickMe</button>
-        </div>
-        <div className="example-card col-4" style={style}>
-          <h2>Card Heading 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            autem vel odit asperiores quae eveniet reprehenderit obcaecati,
-            ipsum consectetur sunt temporibus nam, ab possimus aspernatur
-            repudiandae cumque amet quia numquam!
-          </p>
-          <button className="btn btn-primary">ClickMe</button>
-        </div>
-        <div className="example-card col-3" style={style}>
-          <h2>Card Heading 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            autem vel odit asperiores quae eveniet reprehenderit obcaecati,
-            ipsum consectetur sunt temporibus nam, ab possimus aspernatur
-            repudiandae cumque amet quia numquam!
-          </p>
-          <button className="btn btn-primary">ClickMe</button>
-        </div>
-      </div>
+      <div className="row">{blockCards}</div>
     </Fragment>
   );
 }
