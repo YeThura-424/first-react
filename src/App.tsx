51863a798aa9b7d25cd76aa40ac10da19ecd2d3b
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-15 20:15:41 +0630

    class based component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import Message from "./Message";
 import CardGroup from "./components/CardGroup";
 import ListGroup from "./components/ListGroup";
+import ExtraCard from "./components/ExtraCard";
 
 function App() {
   return (
     <div>
       <ListGroup />
       <CardGroup />
+      <ExtraCard />
     </div>
   );
 }
---
+++ src/components/ExtraCard.tsx
@@ -0,0 +1,54 @@
+import React, { Component, Fragment } from "react";
+
+class Cards extends Component {
+  style = {
+    margin: "16px",
+    padding: "16px",
+    borderRadius: "5px",
+    boxShadow: "0px 0px 9px 4px #ccc4f0",
+  };
+  cardArr = [
+    {
+      title: "Block Title 1",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritati autem vel odit asperiores quae eveniet reprehenderit obcaecati",
+    },
+    {
+      title: "Block Title 2",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritati autem vel odit asperiores quae eveniet reprehenderit obcaecati",
+    },
+    {
+      title: "Block Title 3",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritati autem vel odit asperiores quae eveniet reprehenderit obcaecati",
+    },
+  ];
+  state = {
+    showCard: true,
+  };
+  blogCards = this.cardArr.map((item, index) => {
+    return (
+      <div className="example-card col-3" style={this.style} key={index}>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+        <button className="btn btn-primary">ClickMe</button>
+      </div>
+    );
+  });
+  toggleCards = () => {
+    this.setState({ showCard: !this.state.showCard });
+  };
+  render() {
+    return (
+      <Fragment>
+        <button onClick={this.toggleCards}>
+          {this.state.showCard ? "Hide Card" : "Show Card"}
+        </button>
+        {this.state.showCard ? this.blogCards : null}
+      </Fragment>
+    );
+  }
+}
+
+export default Cards;
