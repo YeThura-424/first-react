9b1bd24d99eb8024d53f35ef90b6d2798cfc4b1b
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-29 12:12:00 +0630

    password generate functionality

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useState } from "react";
 
 const PasswordGenerator = () => {
@@ -6,11 +6,39 @@ const PasswordGenerator = () => {
   const [length, setLength] = useState(8);
   const [numberAllow, setNumbrAllow] = useState(false);
   const [charAllow, setCharAllow] = useState(false);
+  const passwordRef = useRef();
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+
+    let passString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllow) passString += "0123456789";
+    if (charAllow) passString += "!@#$%^&*()[]<>?/";
+
+    for (let i = 1; i < length; i++) {
+      const randNum = Math.floor(Math.random() * passString.length + 1);
+      pass += passString.charAt(randNum);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllow, charAllow]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllow, charAllow]);
+
+  const copyPassword = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  };
+
   return (
     <div className="password-wrapper">
       <div className="upper-section">
-        <input type="text" value={password} />
-        <button className="copy-button">Copy</button>
+        <input type="text" value={password} readOnly ref={passwordRef} />
+        <button className="copy-button" onClick={copyPassword}>
+          Copy
+        </button>
       </div>
       <div className="lower-section">
         <div className="length-section">
@@ -20,6 +48,7 @@ const PasswordGenerator = () => {
             id="length"
             min={1}
             max={100}
+            onChange={(e) => setLength(e.target.value)}
             defaultValue={length}
           />
           <label htmlFor="length">Length : {length} </label>
@@ -30,6 +59,7 @@ const PasswordGenerator = () => {
             id="number"
             name=""
             defaultChecked={numberAllow}
+            onChange={() => setNumbrAllow((prev) => !prev)}
           />
           <label htmlFor="number">Number</label>
         </div>
@@ -39,6 +69,7 @@ const PasswordGenerator = () => {
             id="char"
             name="char"
             defaultChecked={charAllow}
+            onChange={() => setCharAllow((prev) => !prev)}
           />
           <label htmlFor="char">Character</label>
         </div>
